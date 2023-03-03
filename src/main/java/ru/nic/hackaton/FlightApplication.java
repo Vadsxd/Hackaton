9f4a6d42b757b2847d30379efbbab14565817901
Vadsxd <vadim.sannikov.2018@mail.ru>
2023-03-03 10:44:04 +0300

    swagger add

--- src/main/java/ru/nic/hackaton/FlightApplication.java
+++ src/main/java/ru/nic/hackaton/FlightApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlightApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FlightApplication.class, args);
     }
---
+++ src/main/java/ru/nic/hackaton/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package ru.nic.hackaton.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+import static springfox.documentation.service.ApiInfo.DEFAULT_CONTACT;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Hackaton",
+                "Проект на Хаккатон 2023",
+                "1.0",
+                "urn:tos",
+                DEFAULT_CONTACT,
+                "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>()
+        );
+    }
+}
